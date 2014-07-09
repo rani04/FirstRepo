72aebef65817aeaa8234ebc48b37ef2080654193
Toheed Khan <tkhan@ext-personagraph.com>
2014-07-09 12:11:05 +0530

    Project Files added

---
+++ FirstCIApp/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  FirstCIApp
+//
+//  Created by 47Billion-mac-1 on 09/07/14.
+//  Copyright (c) 2014 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ FirstCIApp/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  FirstCIApp
+//
+//  Created by 47Billion-mac-1 on 09/07/14.
+//  Copyright (c) 2014 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
